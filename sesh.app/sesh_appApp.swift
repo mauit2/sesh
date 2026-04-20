c69c5ecf31ed7bf5f5c542ea3cb2d78ecf1384a2
Mauritz Andersson <mauritz0208@gmail.com>
2026-04-20 15:04:09 +0200

    Initial Commit

---
+++ sesh.app/sesh_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  sesh_appApp.swift
+//  sesh.app
+//
+//  Created by Mauritz Andersson on 2026-04-20.
+//
+
+import SwiftUI
+
+@main
+struct sesh_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
